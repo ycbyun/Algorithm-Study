2b263dee10cec29ed7776235ff3272034961711f
ycbyun <chan200399@gmail.com>
2025-07-24 21:47:21 +0900

    [level 0] Title: 카운트 업, Time: 0.04 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int length = end_num -start_num + 1;
+        
+        int[] answer = new int[length];
+        for (int i = 0; i < length; i++) {
+            answer[i] = start_num + i;
+        }
+
+        return answer;
+    }
+}
