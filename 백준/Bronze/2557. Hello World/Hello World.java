7f9f8d6b60b315f02906ec632140d84efb0d1bbc
ycbyun <chan200399@gmail.com>
2025-09-24 21:39:47 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
