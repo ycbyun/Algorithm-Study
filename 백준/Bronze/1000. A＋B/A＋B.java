c35c628ac54a486a1287d551a65ab9643042d437
ycbyun <chan200399@gmail.com>
2025-09-24 21:49:54 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        int c = a + b;
+        
+        System.out.println(c);
+    }
+}
